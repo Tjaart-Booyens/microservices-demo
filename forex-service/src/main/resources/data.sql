8903daac66bab07dcbe9cc9959ef8b1ecc45b276
Tjaart Booyens <tjaartbooyens6@gmail.com>
2020-02-11 13:30:47 +0200

    Added Forex Service implementation

---
+++ forex-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10001,'USD','ZAR',15,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10002,'EUR','ZAR',16,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(10003,'GBP','ZAR',19,0);
